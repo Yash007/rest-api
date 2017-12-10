dd3b6e3c0af15f322dfe08295cc8a0a275d4e592
YASH SOMPURA <yash007@YASHs-MacBook-Pro.local>
2017-12-10 18:43:03 -0500

    solve small bug

--- routes/users.js
+++ routes/users.js
@@ -60,7 +60,7 @@ exports.updateUser = function(req, res) {
     console.log('Updating user: ' + id);
     console.log(JSON.stringify(user));
     db.collection('users', function(err, collection) {
-        collection.update({'_id':new BSON.ObjectID(id)}, user, {safe:true}, function(err, result) {
+        collection.update({'_id':new mongo.ObjectID(id)}, user, {safe:true}, function(err, result) {
             if (err) {
                 console.log('Error updating user: ' + err);
                 res.send({'error':'An error has occurred'});
@@ -76,7 +76,7 @@ exports.deleteUser = function(req, res) {
     var id = req.params.id;
     console.log('Deleting user: ' + id);
     db.collection('users', function(err, collection) {
-        collection.remove({'_id':new BSON.ObjectID(id)}, {safe:true}, function(err, result) {
+        collection.remove({'_id':new mongo.ObjectID(id)}, {safe:true}, function(err, result) {
             if (err) {
                 res.send({'error':'An error has occurred - ' + err});
             } else {
