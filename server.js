8ed4ecea8892014200893bbfe0a4a739b6f00297
YASH SOMPURA <yash007@YASHs-MacBook-Pro.local>
2017-12-10 17:42:59 -0500

    Set proper method for Terminal console messages

--- server.js
+++ server.js
@@ -19,5 +19,5 @@ app.get('/users', users.findAll);
 //GET METHOD FOR GETTING USERS BY ID FROM DATABASE
 app.get('/users/:id', users.findById);
 
-app.listen(3000);
-console.log('Listening on port 3000...');
+app.listen(3000, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:3000/');
