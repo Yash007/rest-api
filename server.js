d8a7e04ddc649d9ff730da67ab8e30924ffadd00
YASH SOMPURA <yash007@YASHs-MacBook-Pro.local>
2017-12-10 17:52:35 -0500

    Added POST, PUT, DELETE method for more operations and also added logger and bodyParser for json Data

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ users = require('./routes/users');
 
 var app = express();
 
+app.configure(function () {
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+});
 
 //GET METHOD FOR GETTING ALL USERS FROM DATABASE
 app.get('/users', users.findAll);
@@ -19,5 +23,11 @@ app.get('/users', users.findAll);
 //GET METHOD FOR GETTING USERS BY ID FROM DATABASE
 app.get('/users/:id', users.findById);
 
+
+app.post('/users', users.addUser);
+app.put('/users/:id', users.updateUser);
+app.delete('/users/:id', users.deleteUser);
+
+//App will listen on port 3000 and http route by 127.0.0.1
 app.listen(3000, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:3000/');
